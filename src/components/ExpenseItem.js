875f43484b9dc36a370924151d78320eaf7a67f5
Tom Wagner <tomwagner@Toms-iMac-2.attlocal.net>
2021-04-14 00:33:34 -0500

    Split Components and added more css

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,13 +1,10 @@
-import './ExpenseItem.css';
+import ExpenseDate from './ExpenseDate'
+import './css/ExpenseItem.css';
 
 function ExpenseItem(props) {
-    const expenseDate = new Date(2021, 2, 28);
-    const expenseTitle = 'Car Insurance';
-    const expenseAmount = 294.67;
-
     return ( 
         <div className="expense-item">
-            <div>{expenseDate.toISOString()}</div>
+            <ExpenseDate date={props.date}/>
             <div className="expense-item__description">
                 <h2>{props.title}</h2>
                 <div className="expense-item__price">${props.amount}</div>
