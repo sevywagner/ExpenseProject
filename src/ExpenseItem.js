d65d38ffb1b3f32c71bedd86fe56103e009845db
Tom Wagner <tomwagner@Toms-iMac-2.attlocal.net>
2021-04-13 23:43:18 -0500

    Used props to create more expense items

--- src/ExpenseItem.js
+++ src/ExpenseItem.js
@@ -1,15 +1,19 @@
 import './ExpenseItem.css';
 
-function ExpenseItem() {
+function ExpenseItem(props) {
+    const expenseDate = new Date(2021, 2, 28);
+    const expenseTitle = 'Car Insurance';
+    const expenseAmount = 294.67;
+
     return ( 
         <div className="expense-item">
-            <div>March 28, 2021</div>
+            <div>{expenseDate.toISOString()}</div>
             <div className="expense-item__description">
-                <h2>Car Insurance</h2>
-                <div className="expense-item__price">$297.57</div>
+                <h2>{props.title}</h2>
+                <div className="expense-item__price">${props.amount}</div>
             </div>
         </div>
-    );
+    )
 }
 
 export default ExpenseItem;
